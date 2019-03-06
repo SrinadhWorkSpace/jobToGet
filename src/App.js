2ae2c4651f3dbadd2ff996e8204b4bd4448b6a07
srinadh rayala <srinadh.rayala@capgemini.com>
2019-03-07 00:14:25 +0530

    code to keep

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SampleTest from './Sampletest.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="AppOfAll">
+        <SampleTest/>
+        <table>
+          <tr><td id="UserNameToFetch">UserName: <input type ="text" /></td></tr>
+          <tr><td id="IdToFetch">Password: <input type ="password" /></td></tr>
+          <tr>
+            <td><button onClick="{this.validatingData}">Submit</button>&nbsp;&nbsp;&nbsp;&nbsp;<button>Reset</button></td>
+          </tr>
+        </table>
       </div>
     );
   }
